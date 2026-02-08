bfccf666139e9b87bd630476bbf1be28c0ee41e4
masya <marinabondarska2006@gmail.com>
2026-02-08 23:05:02 +0100

    change in the solution of the task

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,17 @@
+function filterArray(numbers, value){
+
+  const filteredNumbers = [];
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    
+    if (numbers[i] > value) {
+        filteredNumbers.push(numbers[i]);
+    }
+    
+  } 
+  return filteredNumbers;
+}
+
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
